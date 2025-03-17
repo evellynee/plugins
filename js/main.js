241beceb4cec8b2dca0e8215c015c53b218be147
evellynee <evellynesilva11@gmail.com>
2025-03-17 09:35:35 -0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,50 @@
+$(document).ready(function () {
+
+    $('#carousel').slick({
+        autoplay: true,
+        arrows: false
+    });
+
+    $('#telefone').mask('(00) 00000-0000', {
+        placeholder: '(00) 00000-0000'
+    });
+
+    $('#cpf').mask('000.000.000-00', {
+        placeholder: '000.000.000-00'
+    });
+
+    $('#cep').mask('00000-000', {
+        placeholder: '00000-000'
+    });
+
+    $('form').validate({
+        rules:{
+            nome:{
+                required:true
+            },
+            email:{
+                required:true,
+                email:true
+            },
+            telefone: {
+                required:true
+            },
+            veiculo: {
+                required:true
+            },
+            mensagem:{
+                required:true
+            }
+        },
+        submitHandler: function (form) {
+            console.log(form)
+        },
+        invalidHandler: function (evento, validador) {
+            let camposIncorretos = validador.numberOfInvalids();
+            if (camposIncorretos) {
+                alert(`Existem ${camposIncorretos} campos incorretos`)
+            }
+        }
+    })
+
+});
